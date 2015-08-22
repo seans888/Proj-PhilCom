8fdfe473f0be7fba20cd891c2dab2cab81cb7123
Christine Joy Ferrer <ChristinejoyGD@gmail.com>
2015-08-22 16:50:36 +0800

    Add Sitename.php and SitenameSearch.php in the models folder

---
+++ application/philcom_pms/advanced/frontend/models/Sitename.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "sitename".
+ *
+ * @property integer $id
+ * @property string $sitecode
+ * @property string $sitename
+ *
+ * @property Project[] $projects
+ */
+class Sitename extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'sitename';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['sitecode', 'sitename'], 'required'],
+            [['sitecode', 'sitename'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'sitecode' => 'Sitecode',
+            'sitename' => 'Sitename',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProjects()
+    {
+        return $this->hasMany(Project::className(), ['sitename_id' => 'id']);
+    }
+	
+	 public function getFullSiteName()
+	{
+		return $this->sitename . " (". $this->sitecode. ")" ;
+                
+	}
+}
